11f279a553bc24875b89b5ad0751cbe62ad1223c
ibnucholdun <ibnucholdun023@gmail.com>
2025-09-22 13:46:50 +0700

    feat: add auth with better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,16 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql",
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  // socialProviders: {
+  //   github: {
+  //     clientId: process.env.GITHUB_CLIENT_ID as string,
+  //     clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+  //   },
+  // },
 });
